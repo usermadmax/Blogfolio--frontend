2bc47aecef1c9fe47d74939e34ea7d19f712f826
usermadmax <ganeshaenagandula2005@gmail.com>
2025-06-29 16:56:08 +0530

    Update login.js

--- src/services/login.js
+++ src/services/login.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
-const baseUrl = '/api/login'
+
+const baseApiUrl = import.meta.env.VITE_API_URL // Will use full URL in production
 
 const login = async credentials => {
-  const response = await axios.post(baseUrl, credentials)
+  const response = await axios.post(`${baseApiUrl}/login`, credentials)
   return response.data
 }
 
